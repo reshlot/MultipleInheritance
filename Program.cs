7bae6065400c23ef3e2e4868d348db2d19ecb9f0
HP <reshmaksriet@gmail.com>
2023-07-28 15:15:48 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    public  class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***Smartphone Brand***\n");
+            Smartphone smartphone = new Smartphone("Apple -","iPhone 12");
+            smartphone.Connect();
+            smartphone.Charge(60);
+            Console.WriteLine(smartphone.Display());
+
+            Console.WriteLine("***Laptop Brand***\n");
+            Laptop laptop = new Laptop("Dell -", "XPS 13");
+            laptop.Connect();
+            laptop.Charge(120);
+            Console.WriteLine(laptop.Display());
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Smartphone.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    public class Smartphone : IConnectable, IRechargeable, IDisplayable
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public bool IsConnected { get; set; }
+        public int BatteryLevel { get; set; }
+
+        public Smartphone(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public bool Connect()
+        {
+            // Connect implementation logic
+            IsConnected = true;
+            return IsConnected;
+        }
+
+        public void Charge(int minutes)
+        {
+            // Charge implementation logic
+            BatteryLevel += minutes;
+            if (BatteryLevel > 100)
+            {
+                BatteryLevel = 100;
+            }
+        }
+
+        public string Display()
+        {
+            // Display implementation logic
+            return $"{Brand} {Model} -> Battery Level: {BatteryLevel}%\n";
+        }
+    }
+}
